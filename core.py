37323a45551c7c061671d4037029561020c62ad3
herenti <106109941+herenti@users.noreply.github.com>
2025-04-10 18:18:58 -0700

    Update core.py

--- core.py
+++ core.py
@@ -1,2 +1,29 @@
+import time
+import urllib.request
+import urllib.parse
+import re
+from datetime import datetime
+
+def regex(pattern, x, default):
+        return re.search(pattern, x).group(1) if re.search(pattern, x) else default
+
+
+# BOT COMMANDS
+
 def _say(args):
     return args
+
+
+def _bgtime(args):
+    try:
+        a, b, c = args[0], args[1], args
+        url = 'http://st.chatango.com/profileimg/%s/%s/%s/mod1.xml' % (a,b,c)
+        data = regex('<d>(.*?)</d>', urllib.request.urlopen(url).read().decode(), None)
+        data = int(data)
+        if time.time() > data:
+            ret = 'Expired'
+        else: ret = 'Expires'
+        data = datetime.fromtimestamp(data).strftime(ret + ': Year: %Y - Month: %m - Day: %d')
+        return data
+    except:
+        return 'That user has never had a background, or there is some other error.'
