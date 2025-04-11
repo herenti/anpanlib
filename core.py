35205b6c700a9146f23244272d18a9691f5e9a43
herenti <106109941+herenti@users.noreply.github.com>
2025-04-10 23:20:38 -0700

    Update core.py

--- core.py
+++ core.py
@@ -1,12 +1,15 @@
 import time
 import urllib.request
 import urllib.parse
+import random
 import re
 from datetime import datetime
 
 def regex(pattern, x, default):
         return re.search(pattern, x).group(1) if re.search(pattern, x) else default
 
+manager = {}
+manager["games"] = {}
 
 # BOT COMMANDS
 
@@ -27,3 +30,31 @@ def _bgtime(args):
         return data
     except:
         return 'That user has never had a background, or there is some other error.'
+
+
+def _numbergame(args):
+    try:
+        if args == 'start':
+            try:
+                manager["games"]["numbergame"]
+                return "There is already a game!!"
+            except:
+                _num = random.randint(1,30)
+                manager["games"]["numbergame"] = _num
+                return "You have started the guessing game! Pick a number between 1 and 30."
+        else:
+            _num = manager["games"]["numbergame"]
+            guess = args
+            try:
+                guess = int(args)
+            except:
+                return 'Not a valid guess.'
+            if guess > _num:
+                return 'Too high!'
+            elif guess < _num:
+                return 'Too low!!'
+            else:
+                del manager["games"]["numbergame"]
+                return 'Thats it!!!'
+    except:
+        return 'You must start the game first! "numbergame start"'
