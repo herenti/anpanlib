c8e09f85b5db224241a8dfa754133e5f00026f50
herenti <106109941+herenti@users.noreply.github.com>
2025-04-13 18:19:16 -0700

    Update core.py

--- core.py
+++ core.py
@@ -36,6 +36,14 @@ slot_machine = {'Anpan': [0.1,1000000],
 def _say(message, args):
     return message
 
+def _fart(message, args):
+    choice = ['thbt','tttbbbbtt', 'tttttthhhhhhbbbbbttt', 'flubflublbblb','brrrrrraapppp','brrrurnnnntttt', ' ']
+    _num = random.randrange(1, 5)
+    choice = [random.choice(choice) for x in range(_num)]
+    choice = ''.join(choice)
+    if choice == ' ':
+        choice = '   - It was a silent one....'
+    return ''.join(choice)
 
 def _bgtime(message, args):
     try:
@@ -91,7 +99,76 @@ def _numbergame(message, args):
         return 'You must start the game first! The command is [numbergame start]'
 
 
-#def _dicebet(message):
+def _dice(message, args):
+    x = message.split(' ',1)
+    try:
+        if len(x) > 1:
+            command, number = x[0], int(x[1])
+        else:
+            command = x[0]
+    except:
+        return "That is not a valid command"
+    user = args['user']
+    chat = args["chat"]
+    try:
+        derp = manager["games"][user]
+    except:
+        return 'You must register for games first. Use the [register] command.'
+    wallet = manager["games"][user]["wallet"]
+    title = manager["games"][user]["title"]
+    amount = 250
+    if command == 'start':
+        try:
+            manager["game_session"][chat]
+            return "There is already a game in this chat. Please finish it first!!"
+        except:
+            _num = random.randint(1,7)
+            try:
+                manager["game_session"][chat]["dice"]
+            except KeyError:
+                manager["game_session"][chat] = {}
+                manager["game_session"][chat]["dice"] = {}
+                manager["game_session"][chat]["dice"]["roll"] = _num
+                manager["game_session"][chat]["dice"]["users"] = {}
+                manager["game_session"][chat]["dice"]["pot"] = 0
+                return title+', you have started a round of dice betting. Up to seven users may play. Minimum of two. Place bets with this command on numbers 1-7 of the dice sides: example: [dice bet 6]. When all players are ready use the command [dice roll].'
+    elif command == 'bet':
+        if number > 7:
+            return "That is not a valid number."
+        elif number < 1:
+            return "That is not a valid number."
+        if user in manager["game_session"][chat]["dice"]["users"]:
+            return "You have already bet on the dice this round."
+        elif number in list(manager["game_session"][chat]["dice"]["users"].values()):
+            return "A user has already bet on that number."
+        elif amount > wallet:
+            return "You dont have enough points in your wallet. [Wallet: %s points]"  % wallet
+        elif len(manager["game_session"][chat]["dice"]["users"]) == 7:
+            return "The game is full. Please use the command [dice roll]"
+        else:
+            manager["games"][user]["wallet"] -= amount
+            manager["game_session"][chat]["dice"]["pot"] += amount
+            manager["game_session"][chat]["dice"]["users"][user] = number
+            return "You have bet %s on the number %s." % (amount, number)
+    elif command == "roll":
+        if user not in manager["game_session"][chat]["dice"]["users"]:
+            return "You are not playing this game. Place a bet first."
+        elif len(manager["game_session"][chat]["dice"]["users"]) < 2:
+            return "There must be at least two users playing."
+        else:
+            win_num = manager["game_session"][chat]["dice"]["roll"]
+            if win_num not in list(manager["game_session"][chat]["dice"]["users"].values()):
+                del manager["game_session"][chat]
+                return "The winning number is %s. A user did not win. The bot takes the pot." % win_num
+            winner = [i for i in manager["game_session"][chat]["dice"]["users"] if manager["game_session"][chat]["dice"]["users"][i] == win_num][0]
+            manager["games"][winner]["wallet"] += manager["game_session"][chat]["dice"]["pot"]
+            winner = manager["games"][winner]["title"]
+            pot = manager["game_session"][chat]["dice"]["pot"]
+            del manager["game_session"][chat]
+            return "The winner is " + winner  + '!!!! They win the pot of '+ str(pot) + ' points. The winning number was '+ str(win_num) + '.'
+
+
+
     #each player bets a certain amount of points on a dice number, same for every player, winner takes all. cpu takes number not bet on for jackpot pool. maybe have a player wallet for betting games. one number rarest gives chance for jackpot. player can earn title with points.
 
 def _register(message, args):
@@ -102,7 +179,7 @@ def _register(message, args):
     except:
         manager["games"][user] = dict(
             wallet = 1000,
-            title = None,
+            title = user,
             items = []
         )
         return "Registered for the games. Your starting wallet is 1000 points."
@@ -138,7 +215,7 @@ def _slot(message, args):
     for i in result_num:
         if i[1] < 2:
             result_num.remove(i)
-    final_results = '['+', '.join(outcome)+'] '
+    final_results = '<b>[</b> '+' <b>|</b> '.join(outcome)+' <b>]</b> '
     for i in result_num:
         if i[1] == 3:
             amount = slot_machine[i[0]][1]*2
@@ -147,7 +224,7 @@ def _slot(message, args):
             if i == 'Anpan':
                 return final_results+'You hit the big Anpan jackpot!! You win '+str(amount)+' points. [<b>-100</b> points for playing]'
             else:
-                return final_results+'You hit a three of a kind with ['+i[0]+']!!! You win '+str(amount)+' points. [<b>-100</b> points for playing]'
+                return final_results+'You hit a three of a kind with <b>[</b> '+i[0]+' <b>]</b>!!! You win '+str(amount)+' points. [<b>-100</b> points for playing]'
         elif i[1] == 2:
             outcome.remove(i[0])
             outcome.remove(i[0])
@@ -161,7 +238,7 @@ def _slot(message, args):
                 amount = 1
             wallet += amount
             manager["games"][user]["wallet"] = wallet
-            return final_results+'You hit a two of a kind with ['+i[0]+']!!! You win '+str(amount)+' points. [<b>-100</b> points for playing]'
+            return final_results+'You hit a two of a kind with <b>[</b> '+i[0]+' <b>]</b>!!! You win '+str(amount)+' points. [<b>-100</b> points for playing]'
         else:
             continue
     amount = [slot_machine[i][1]*(1/20) for i in outcome]
